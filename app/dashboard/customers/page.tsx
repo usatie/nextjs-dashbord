b6d071e3fa86d858f5d026790c50f48ade9f6d6b
Shun Usami <usatie@users.noreply.github.com>
2023-11-03 15:04:02 +0900

    Implement /dashboard/customers (#8)

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,25 @@
 import { Metadata } from 'next';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
 
 export const metadata: Metadata = {
 	title: 'Customers',
 }
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+	searchParams,
+}: {
+	searchParams?: {
+		query?: string;
+		page?: string;
+	};
+}) {
+	const query = searchParams?.query || '';
+	const customers = await fetchFilteredCustomers(query);
+
+	return (
+		<main>
+			<CustomersTable customers={customers} />
+		</main>
+	)
 }
